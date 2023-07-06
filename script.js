8c99ce870c119f6927123b2075459d4aaa97d767
manishk168 <135124350+manishk168@users.noreply.github.com>
2023-07-06 12:40:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const list = document.querySelector('ul');
+// const anchor = document.querySelector('a');
+
+function myFunction(){
+    list.classList.toggle('show');
+    // anchor.classList.toggle('shown');
+};
+
+
+
